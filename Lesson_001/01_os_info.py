7c92887234fe96c2a9ac858145f65d368f692da3
Darina Mukhortova <arinenumb@yandex.ru>
2020-09-28 14:10:04 +0700

    убран TODO

--- Lesson_001/01_os_info.py
+++ Lesson_001/01_os_info.py
@@ -2,11 +2,8 @@
 
 # Нужно собрать информацию об операционной системе и версии пайтона
 
-# TODO запустить этот скрипт и закомитить его работы (файл os_info.txt)
-
 import platform
 import sys
-
 info = 'OS info is \n{}\n\nPython version is {} {}'.format(
     platform.uname(), sys.version, platform.architecture())
 print(info)
